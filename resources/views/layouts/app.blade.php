97c4cb01de71cbcf965438690af3f74332ccb77e
@CristianEAC <cristian.arce.ec@gmail.com>
2025-07-24 23:42:31 -0400

    veinte subida

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,3 +3,14 @@
 @section('content')
     @yield('content')
 @endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+@endsection
+
+@section('js')
+    <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+@endsection
