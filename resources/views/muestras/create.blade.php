e59e4ce94ff33a0394b2ee9299e0d560d74cb73d
@CristianEAC <cristian.arce.ec@gmail.com>
2025-07-25 18:44:57 -0400

    veinte cuatro subida

--- resources/views/muestras/create.blade.php
+++ resources/views/muestras/create.blade.php
@@ -173,7 +173,6 @@
 @stop
 
 @section('css')
-    @vite('resources/css/app.css')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <style>
         /* Para que select2 se integre bien con AdminLTE */
